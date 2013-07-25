de9812f6493a3dfa4895b486fc658207bb76f606
rodel <rodel@vmiprog1>
2013-07-25 15:01:07 +0800

    Added Test.java

--- GitProject/src/git/practice/Test.java
+++ GitProject/src/git/practice/Test.java
@@ -11,5 +11,6 @@ package git.practice;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Another changes.");
     }
 }
