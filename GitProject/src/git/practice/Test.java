9b439542232e248d2b97330111978b07ba7a603b
rodel <rodel@vmiprog1>
2013-07-25 11:56:46 +0800

    Added Test.java

---
+++ GitProject/src/git/practice/Test.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git.practice;
+
+/**
+ *
+ * @author rodel
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
